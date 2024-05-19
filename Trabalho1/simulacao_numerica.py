f165adcdf92f2884621b54aea5aa497fb59d4a05
wagaojr <wagnerjunior@poli.ufrj.br>
2024-05-19 17:48:46 -0300

    added simulacao numerica

--- Trabalho1/simulacao_numerica.py
+++ Trabalho1/simulacao_numerica.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import control as ctrl
+import numpy as np
 
 # Definir a função de transferência
 # Exemplo: G(s) = (100/16) [(s^2 + 16)/(s^2 + 0.2s + 100)]
@@ -13,4 +14,23 @@ system = ctrl.TransferFunction(numerator, denominator)
 mag, phase, omega = ctrl.bode(system, dB=True, Hz=False, deg=True, plot=True)
 
 # Exibir os gráficos
+#plt.show() # gráfico do bode
+
+time = np.linspace(0, 10, 1000)
+
+frequencia = 100
+cos_input = np.cos(frequencia * time)
+
+time, resposta = ctrl.forced_response(system, T=time, U=cos_input)
+
+plt.figure()
+plt.plot(time, cos_input, label='Entrada de Cosseno')
+plt.plot(time, resposta, label='Resposta do Sistema')
+plt.title('Resposta à Entrada de Cosseno')
+plt.xlabel('Tempo (s)')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.grid(True)
 plt.show()
+
+
